8ece2305caa33f23f53524819e343167db2c955c
Cheikh-Kanteye <cheikhkanteye.contact@gmail.com>
2026-01-01 15:49:19 +0000

    .

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen w-screen bg-gray-100 flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-          CarePulse
-        </h1>
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">CarePulse</h1>
         <p className="text-xl text-gray-700">
           Bienvenue sur votre application de gestion de santé
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
